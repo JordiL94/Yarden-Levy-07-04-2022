b803091e8d60ca5e5dac969e0e4e53937c5af3f4
Mansei <yarden835@gmail.com>
2022-04-11 17:22:31 +0300

    code cleaning

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,30 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 
+import { loadFavorites, removeFromFavorites } from '../store/weather.action';
 import { SearchBar } from '../cmps/SearchBar';
 import { WeatherList } from '../cmps/weather/WeatherList';
 
-function _Favorites() {
-
-    const [weatherInfo, setWeatherInfo] = useState(null);
+function _Favorites(props) {
+    const { favorites, loadFavorites, removeFromFavorites, isDarkMode, isFarenheit } = props;
 
     const onSearch = (val) => {
-        return;
+        
     }
 
     return (
-        <section className="favorites">
+        <section className={isDarkMode ? "favorites" : "favorites dark"}>
             <SearchBar onSearch={onSearch} placeholder="Search favorites" />
-            <WeatherList weatherInfo={weatherInfo} />
+            <WeatherList weatherList={favorites} isFarenheit={isFarenheit} />
         </section>
     )
 }
 
-function mapStateToProps({ WeatherModule }) {
-
+function mapStateToProps({ weatherModule, userModule }) {
+    return {
+        favorites: weatherModule.favorites,
+        isDarkMode: userModule.isDarkMode,
+        isFarenheit: userModule.isFarenheit
+    }
 }
 
 const mapDispatchToProps = {
+    loadFavorites,
+    removeFromFavorites
 }
 
 export const Favorites = connect(mapStateToProps, mapDispatchToProps)(_Favorites);
